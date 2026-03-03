02e4fd5399b26c06e2e4e56292593760532dbfc2
Beñat <zabalaortizbezo@gmail.com>
2026-03-03 14:51:01 +0100

    Comienzos Numpy

---
+++ prueba01.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+a = np.array([1,4,6,9])
+b = np.array([2,3,7,8])
+c = np.array([12,32,16,28,35])
+print(a + b)
+print(a.shape)
+print(b.size)
+print(c.max())
+d =  b * a
+print(d)
+print(d.mean())
